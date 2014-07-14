e6cfa060c23af5c1d32a96e99cf3ef5167577b62
habakook <habakook@users.noreply.github.com>
2014-07-13 22:15:05 -0700

    add new zohar translation

--- zohar/urls.py
+++ zohar/urls.py
@@ -5,5 +5,5 @@ from zohar import views
 urlpatterns = patterns('',
 url(r'^$', views.index, name='index'),
 # ex: /zohar/5/
-url(r'^(?P<book_number>\d+)/$', views.book, name='view_book'),
+#url(r'^(?P<book_number>\d+)/$', views.book, name='view_book'),
 )

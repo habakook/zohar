6523851875dd134a6a8ec2e28d24ace658c8fc81
habakook <habakook@habakooks-MacBook-Air.local>
2017-12-24 07:52:20 -0800

    fix url forwarding

--- kab/urls.py
+++ kab/urls.py
@@ -5,5 +5,6 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('zohar.urls')),
+    url(r'^zohar/', include('zohar.urls')),
+    url(r'', include('zohar.urls'))
 ]

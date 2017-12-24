9e9c1ba6fdefb12e2428857ddd99dd0492350914
habakook <habakook>
2017-12-24 08:09:06 +0000

    adjust pe3

--- kab/settings.py
+++ kab/settings.py
@@ -24,6 +24,25 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
+# TEMPLATE_DIRS = (
+#   '/home/habakook/zohar/zohar/templates/zohar/',
+# )
+TEMPLATES = [
+ {
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [os.path.join(BASE_DIR, 'templates')],
+    'APP_DIRS': True,
+    'OPTIONS': {
+        'context_processors': [
+            'django.template.context_processors.debug',
+            'django.template.context_processors.request',
+            'django.contrib.auth.context_processors.auth',
+            'django.contrib.messages.context_processors.messages',
+        ],
+    },
+ },
+]
+
 ALLOWED_HOSTS = []
 
 
--- kab/urls.py
+++ kab/urls.py
@@ -1,13 +1,21 @@
-from django.conf.urls import patterns, include, url
+# from django.conf.urls import patterns, include, url
+
+# from django.contrib import admin
+# admin.autodiscover()
+
+# urlpatterns = patterns('',
+#     # Examples:
+#     # url(r'^$', 'kab.views.home', name='home'),
+#     # url(r'^blog/', include('blog.urls')),
+#     url(r'^admin/', include(admin.site.urls)),
+#     url(r'^zohar/', include('zohar.urls')),
+# )
+from django.conf.urls import include, url
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'kab.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^zohar/', include('zohar.urls')),
-)
+]

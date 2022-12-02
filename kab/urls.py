e983d972a3affd173e4e11f1a10464a6f27a55de
habakook <habakook@users.noreply.github.com>
2022-12-02 00:08:46 -0800

    Update urls.py
    5

--- kab/urls.py
+++ kab/urls.py
@@ -4,7 +4,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^zohar/', include('zohar.urls')),
-    url(r'', include('zohar.urls'))
+#     url(r'^admin/', include(admin.site.urls)),
+#     url(r'^zohar/', include('zohar.urls')),
+#     url(r'', include('zohar.urls'))
+    url(r'^admin/', admin.site.urls),
+    url(r'^zohar/', zohar.urls),
+    url(r'', zohar.urls)
 ]

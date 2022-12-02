f5aba15b0e352f076eaacf6d2707296fc41b779e
habakook <habakook@users.noreply.github.com>
2022-12-02 00:36:03 -0800

    Update urls.py
    11

--- kab/urls.py
+++ kab/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 #     url(r'^admin/', include(admin.site.urls)),
 #     url(r'^zohar/', include('zohar.urls')),
 #     url(r'', include('zohar.urls'))
-    re_path(r'^admin/', admin.site.urls),
-    re_path(r'^zohar/', index),
-    re_path(r'', index)
+    url(r'^admin/', admin.site.urls),
+    url(r'^zohar/', index),
+    url(r'', index)
 ]
